348c24d39bf1598666b82db60f73aa6456b8e705
kartikm16 <mahamunikartik007@gmail.com>
2025-09-26 20:53:50 +0530

    Ml integrated

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ import firebase_admin
 from firebase_admin import credentials,db
 app=Flask("__name__")
 
-sampledata=pd.DataFrame([[30, 40 , 1000, 200000, 5]],columns=['attendance', 'exam_score', 'fees_pending', 'family_income', 'backlogs'])
 model=pickle.load(open("student_dropout_model_3levels.pkl","rb"))
-# data = np.array([70, 80, 1000, 200000, 2])
-# data = data.reshape(1, -1)  # 1 sample, 5 features
+
 base_dir=os.path.dirname(os.path.abspath(__file__))
 cred_path=os.path.join(base_dir,"tech810servicekey.json")
 cred=credentials.Certificate(cred_path)
@@ -19,10 +17,9 @@ firebase_admin.initialize_app(cred,{
 })
 
 
-# new_predictions=model.predict(sampledata)
+
 risk_labels = {0: 'Low Risk', 1: 'Medium Risk', 2: 'High Risk'}
-predicted_class = model.predict(sampledata)[0]
-print(risk_labels[predicted_class])
+
 
 @app.route('/')
 def home():
@@ -41,16 +38,30 @@ def predict():
     ref=db.reference('students')
     data=ref.get()
     if isinstance(data, dict):
-        values = data.values()
+        values = data.items()
     elif isinstance(data, list):
-        values = [item for item in data if item is not None]
+        values = [(i, item) for i, item in enumerate(data) if item is not None]
+
     else:
         values = []
-    for value in values:
+    for key,value in values:
         total_marks=0
         for mark in value['marks'].values():
             total_marks=total_marks+mark
-        print("total marks:",total_marks)
+        exam_score=total_marks/400 * 100
+        attendance=value['attendance']
+        family_income=value['family_income']
+        fees_pending=value['fees_pending']
+        kt=value['kt']
+        student_data=pd.DataFrame([[attendance, exam_score , fees_pending, family_income, kt]],columns=['attendance', 'exam_score', 'fees_pending', 'family_income', 'backlogs'])
+        risk=risk_labels[model.predict(student_data)[0]]
+        ref = db.reference(f'students/{key}')  # replace with actual student key
+        ref.update({
+        "risk_status":risk,
+        "risk":None
+       })
+
+
 
 
     return jsonify(data)
